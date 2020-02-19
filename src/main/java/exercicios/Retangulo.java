03f19cbff47f1dc4282ba51ea2daa7f322ccfd86
unknown <cezargodoy10@gmail.com>
2020-02-19 20:58:51 -0300

    Classe Retangulo

---
+++ src/main/java/exercicios/Retangulo.java
@@ -0,0 +1,15 @@
+package exemplos;
+
+public class Retangulo {
+    public Double base;
+    public Double altura;
+
+    public Doub
+    le calcularArea() {
+        return base * altura;
+    }
+
+    public Double calcularPerimetro(){
+        return 2 * (base + altura);
+    }
+}
