58c51736954d8d3f420a23ce0865539328ff2b6a
Cezar Godoy <60990343+cezargodoy@users.noreply.github.com>
2020-02-25 12:51:57 -0300

    Atividade 1 Cezar Main

---
+++ src/main/java/Correntista.java
@@ -0,0 +1,21 @@
+package exercicios;
+
+public class Correntista {
+    public Integer codigo;
+    public String nome;
+    public String sobrenome;
+    public String email;
+    public String telefone;
+
+    public Integer getCodigo() {
+        return codigo;
+    }
+
+    public String getNome() {
+        return nome + " " + sobrenome;
+    }
+
+    public String getEmail;{ return email; }
+
+    public String getTelefone; { return telefone; }
+}
---
+++ src/main/java/Retangulo.java
@@ -0,0 +1,15 @@
+package exercicios;
+
+public class Retangulo {
+
+    public Double base;
+    public Double altura;
+
+    public Double calcularArea();
+        return base *altura;
+    }
+    public Double calcularPerimetro() {
+        return (2 * base) + (2 * altura);
+    }
+
+}
